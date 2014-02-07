884e88f87ea06950baa21b3361655246013164c0
Jim Heising <jheising@redfoundry.com>
2014-02-07 09:12:24 -0800

    Added testing

---
+++ test/node-client-test.js
@@ -0,0 +1,12 @@
+var should = require("should");
+var dweetIOClient = require("./../dweet.io.js");
+
+var dweetio = new dweetIOClient();
+
+describe("dweetio-client", function()
+{
+	it("should have a version with the format #.#.#", function()
+	{
+		
+	});
+});
